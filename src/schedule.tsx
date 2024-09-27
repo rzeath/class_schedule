b99685b91c629364e10e9941deaa39d979b9cdc3
rzeath <109345183+rzeath@users.noreply.github.com>
2024-09-27 11:49:16 +0800

    Create schedule.tsx

---
+++ src/schedule.tsx
@@ -0,0 +1,42 @@
+function minutesToHours(minutes:number){
+    let hours:number = Math.floor(minutes / 60);
+    let mins:number = minutes % 60;
+
+    hours = hours % 12;
+    if (hours ==0) hours = 12;
+    let isPM:boolean = minutes >= 60 * 12;
+
+    let timeStr:string = hours + ":" + (mins < 10 ? "0" + mins : mins);
+    return isPM ? timeStr +"pm" : timeStr + "am";
+}
+
+export function paintCalendar(){
+    // let savedClasses : any = getSavedClasses () || {};
+    const minStartTime: number = 8 * 60; // 8am
+    const maxEndTime: number = 15 * 60; // 3pm
+
+    const startHourMins: number = Math.floor(minStartTime / 60) * 60;
+    const endHourMins: number = Math.ceil(maxEndTime / 60) * 60;
+
+    const scaleFactor: number = 1.2;
+    const calendar = document.getElementById('time-calendar');
+
+    if(calendar){
+        while (calendar.firstChild){
+            calendar.removeChild(calendar.firstChild);
+        }
+
+        calendar.style.height = ((scaleFactor * (endHourMins - startHourMins) + 120)) + 'px';
+
+        //add all of the hr element for hour lines
+        for (let i = startHourMins; i<= endHourMins; i += 60){
+            const hourLine = document.createElement('div');         
+            hourLine.className = 'hour-line';
+            hourLine.style.top = (i - startHourMins) * scaleFactor + 'px';
+            hourLine.setAttribute('data-hour', minutesToHours(i).split(':')[0]+':00 - '+ minutesToHours(i + 60).split(':')[0]+ ':00');
+            calendar.appendChild(hourLine);                                                                                                                                               
+        }
+    }else{
+        console.error("Calendar element not found");
+    }
+}
